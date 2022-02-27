cd8c80e6b78956d34534c2a7fa080b854ab10a9d
carlinho11 <38302156+carlinho11@users.noreply.github.com>
2022-02-27 17:58:50 -0300

    Delete unity_config.dart

--- lib/app/shared/utils/unity_config.dart
+++
@@ -1,7 +0,0 @@
-class UnityConfig {
-  UnityConfig._();
-
-  static String placementId = 'Banner_Android';
-  static String gameId = '4621027';
-  static String URL = 'https://apidesenholivre.herokuapp.com/api/acesso';
-}
